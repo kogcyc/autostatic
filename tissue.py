802fddf6e115601abf6943f8603d2ce8216edc06
kogcyc <kogcyc@gmail.com>
2025-04-24 14:56:54 -0700

    Update tissue.py

--- tissue.py
+++ tissue.py
@@ -36,7 +36,7 @@ for md_path in MARKDOWN_DIR.rglob("*.md"):
     # Select template
     template_name = page.get("template", "default") + ".html"
 
-    print("\n\n template_name \n\n")
+    print(f"\n\n {template_name} \n\n")
 
     template = env.get_template(template_name)
 
