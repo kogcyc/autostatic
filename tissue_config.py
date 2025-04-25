27d1f8fecff0f170441c2fd59db518670cdbf4b9
kogcyc <kogcyc@gmail.com>
2025-04-25 11:36:59 -0700

    Update tissue_config.py

--- tissue_config.py
+++ tissue_config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Directory paths
 ROOT_DIR = Path(__file__).resolve().parent
-MARKDOWN_DIR = ROOT_DIR / "source"
-BUILD_DIR = ROOT_DIR / "build"
+MARKDOWN_DIR = ROOT_DIR / "markdown"
+BUILD_DIR = ROOT_DIR / "public"
 TEMPLATE_DIR = ROOT_DIR / "templates"
 STATIC_DIR = ROOT_DIR / "static"
